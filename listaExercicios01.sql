de32cb16b736c94a9f072af1e7d73c47eeca77c0
Murilo0123 <98995528+Murilo0123@users.noreply.github.com>
2023-09-03 14:20:47 -0300

    Update listaExercicios01.sql

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -4,3 +4,41 @@ select titulo from livros;
 --2
 select nome from autores
 where (nascimento < '1900-01-01');
+
+--3
+select livros.titulo from livros
+inner join autores on autores.nome = 'J.K. Rowling';
+
+--4
+
+--5
+
+--6
+
+--7
+
+--8
+
+--9
+
+--10
+
+--11
+
+--12
+
+--13
+
+--14
+
+--15
+
+--16
+
+--17
+
+--18
+
+--19
+
+--20
