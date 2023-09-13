e0dc30d897d7435d739dcbbc6da83e56994be152
Murilo0123 <98995528+Murilo0123@users.noreply.github.com>
2023-09-13 18:10:40 -0300

    ex2

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -8,4 +8,13 @@ begin
 //
 call sp_ListarAutores();
 //
+create procedure sp_LivrosPorCategoria(in cat_nome varchar(100))
+begin
+	select Titulo
+    from livro join categoria
+    where categoria.Categoria_ID = livro.Categoria_ID and categoria.Nome = cat_nome;
+end;
+//
+call sp_LivrosPorCategoria('Ciência');
+//
 delimiter ; 
