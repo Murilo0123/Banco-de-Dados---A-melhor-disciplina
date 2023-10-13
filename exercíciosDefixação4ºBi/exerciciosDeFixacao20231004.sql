efdaf0f7650fdb55cc2495e2a30e3bd407337cd8
Murilo0123 <98995528+Murilo0123@users.noreply.github.com>
2023-10-13 18:56:35 -0300

    ex1

--- exercíciosDefixação4ºBi/exerciciosDeFixacao20231004.sql
+++ exercíciosDefixação4ºBi/exerciciosDeFixacao20231004.sql
@@ -1,1 +1,34 @@
 --ex1
+delimiter //
+
+create function total_livros_por_genero(n_genero varchar(100))
+returns int
+deterministic
+begin
+    declare v_quantlivro int;
+    declare done boolean default false;
+
+    declare cursor_quantlivro cursor for 
+        select count(l.titulo)
+        from livro as l
+        inner join genero as g on l.id_genero = g.id
+        where g.nome_genero = n_genero;
+
+    declare continue handler for not found set done = true;
+
+    open cursor_quantlivro;
+
+    fetch_loop: loop
+        fetch cursor_quantlivro into v_quantlivro;
+        if done then
+            leave fetch_loop;
+        end if;
+    end loop fetch_loop;
+
+    close cursor_quantlivro;
+
+    return v_quantlivro;
+end //
+//
+select total_livros_por_genero("Romance");
+//
