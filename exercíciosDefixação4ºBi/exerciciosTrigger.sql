35a11fb348ef1b5806831ff712ce0f12759b91a4
Murilo0123 <98995528+Murilo0123@users.noreply.github.com>
2023-10-25 18:10:46 -0300

    ex2

--- exercíciosDefixação4ºBi/exerciciosTrigger.sql
+++ exercíciosDefixação4ºBi/exerciciosTrigger.sql
@@ -10,3 +10,16 @@ values ("Cleberson");
 
 select * from clientes;
 select * from auditoria;
+
+-- ex2
+create trigger trigger_mensagem_delete
+before delete on clientes 
+for each row
+insert into auditoria ( mensagem, data_hora)
+values ("Nome deletado", now());
+
+delete from clientes 
+where id = 1;
+
+select * from clientes;
+select * from auditoria;
