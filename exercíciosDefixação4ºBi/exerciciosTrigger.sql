41a8e75a41505d1c017b7b9e4b3df89f5e687d1a
Murilo0123 <98995528+Murilo0123@users.noreply.github.com>
2023-10-25 21:02:53 -0300

    ex3

--- exercíciosDefixação4ºBi/exerciciosTrigger.sql
+++ exercíciosDefixação4ºBi/exerciciosTrigger.sql
@@ -23,3 +23,20 @@ where id = 1;
 
 select * from clientes;
 select * from auditoria;
+
+-- ex3
+create trigger trigger_mensagem_update
+after update on clientes
+for each row
+insert into auditoria (mensagem, data_hora)
+values (concat('O nome ', old.nome, ' foi atualizado para ', new.nome), now());
+
+drop trigger trigger_mensagem_update;
+
+update clientes
+set nome = "Hugo"
+where id = 4;
+
+select * from clientes;
+select * from auditoria;
+
